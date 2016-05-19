76e2a74f489133c5d4157c4b4c7a93e967ca7b30
Shidan Xu <shidanxu@mit.edu>
2016-05-19 06:17:27 -0400

    vfinal

--- Final Code/hypothesis.py
+++ Final Code/hypothesis.py
@@ -17,6 +17,10 @@ df = pd.read_pickle("../datasets/mergedWithCluster.pickle")
 
 grouped = df.groupby("cluster").mean()
 
+counted = df.groupby("cluster").count()
+print "Counted: \n"
+print counted
+
 grouped.loc['AVG'] = [13.573331, 14.086646, 2.951581, 41.712824, -74.101604, 0.274385, 0.725615, 1847.931331, 20.645197]
 print grouped
 grouped['numberSessions'].plot(kind = 'bar')
