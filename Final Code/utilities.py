2bdd4cd9af6bbc5f9589e6c1e609d72d28eac878
Shidan Xu <shidanxu@mit.edu>
2016-05-20 11:47:40 -0400

    asf

--- Final Code/utilities.py
+++ Final Code/utilities.py
@@ -141,7 +141,7 @@ def generateDataFromMarkovMatrix(markovMatrix, period = 20):
 	# print output, len(output)
 	return output
 # Evaluate 1 compares the distribution of number of transitions each day for both the generated and testing actual data
-def evaluate1(trainingSetNumTransitions, dailyStates, size = 10000, basepath = '../../../alllogs/'):
+def evaluate1(trainingSetNumTransitions, dailyStates, size = 100000, basepath = '../../../alllogs/'):
 	print "length of dailyStates,", len(dailyStates)
 	print "size: ", size
 	assert len(dailyStates) == size
@@ -212,7 +212,7 @@ def countTransitions(dayStates):
 			currentState = state
 	return total
 
-def doMarkovNaive(testSampleSize = 10000):
+def doMarkovNaive(testSampleSize = 100000):
 	idToCluster = pd.read_pickle("../datasets/mergedWithCluster.pickle")
 
 	totalTransitionMatrix = np.matrix([[0,0,0], [0,0,0], [0,0,0]])
